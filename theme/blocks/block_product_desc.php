3b215d71ad5ce9c665d49a22e4a7ae9ee1a4e24b
duonglq.dev <duonglq.dev@gmail.com>
2025-07-23 14:05:44 +0700

    Add ACF block for product description with repeater fields for content and products

--- theme/blocks/block_product_desc.php
+++ theme/blocks/block_product_desc.php
@@ -14,7 +14,8 @@
 					the_row();
 					$i++;
 					?>
-					<div class="col-span-1 flex flex-col gap-10 <?= $i % 2 == 0 ? 'lg:flex-col-reverse' : '' ?>">
+					<div
+						class="col-span-1 flex flex-col gap-10 <?= $i % 2 == 0 ? 'lg:flex-col-reverse' : '' ?>">
 						<?php echo wp_get_attachment_image( get_sub_field( 'img' ), 'full', false, [ 'class' => 'rounded-[35px] size-full object-cover' ] ) ?>
 						<?php if ( get_sub_field( 'desc' ) ) : ?>
 							<div class="lg:text-xl text-[9px] text-[#525256]">
@@ -27,42 +28,59 @@
 		<?php endif; ?>
 
 	</div>
-	<div class="mt-[30px] relative">
-		<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/bgsam2.png" alt=""
-			loading="lazy"
-			class="absolute left-0 bottom-0 object-contain select-none 1.5xl:block hidden">
-		<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/bgsam1.png" alt=""
-			loading="lazy"
-			class="absolute right-0 top-0 object-contain select-none 1.5xl:block hidden">
-		<div class="container">
-			<div
-				class="md:flex flex-wrap gap-[6px] block_slider watch_css center_items md:mx-0 -mx-5">
-				<?php for ( $i = 0; $i < 4; $i++ ) : ?>
-					<div
-						class="block_slider-item <?= $i === 1 ? 'md:mt-[150px]' : '' ?> <?= $i === 2 ? 'md:-mt-[150px]' : '' ?> md:w-[calc(50%-3px)] w-[83%] h-fit">
-						<div class="group relative xl:pt-12 lg:pt-8 pt-6 px-6 lg:rounded-[35px] rounded-[20px] overflow-hidden aspect-[814/640] bg-no-repeat bg-cover bg-center"
-							style="background-image: url('<?php echo get_stylesheet_directory_uri() ?>/assets/images/home8.png;')">
-							<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/home5.png"
-								alt="" loading="lazy"
-								class="absolute inset-0 size-full lg:opacity-0 group-hover:opacity-100 group-hover:scale-105 transition-all duration-300">
-							<div class="relative z-[1] flex flex-col items-center">
-								<div
-									class="max-w-[332px] xl:text-[40px] lg:text-2xl text-xl lg:leading-[1] tracking-[-1px] text-[#000000E0] text-center">
-									Kiềm Thánh Gióng (pH10)
+	<?php if ( have_rows( 'list_product' ) ) : ?>
+		<div class="mt-[30px] relative">
+			<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/bgsam2.png" alt=""
+				loading="lazy"
+				class="absolute left-0 bottom-0 object-contain select-none 1.5xl:block hidden">
+			<img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/bgsam1.png" alt=""
+				loading="lazy"
+				class="absolute right-0 top-0 object-contain select-none 1.5xl:block hidden">
+			<div class="container">
+				<div
+					class="md:flex flex-wrap gap-[6px] block_slider watch_css center_items md:mx-0 -mx-5">
+					<?php
+					$i = 0;
+					while ( have_rows( 'list_product' ) ) :
+						the_row();
+						$i++;
+						?>
+						<div
+							class="<?= $i ?> block_slider-item <?= $i === 2 ? 'md:mt-[150px]' : '' ?> <?= $i === 3 ? 'md:-mt-[150px]' : '' ?> md:w-[calc(50%-3px)] w-[83%] h-fit">
+							<div class="group relative xl:pt-12 lg:pt-8 pt-6 px-6 lg:rounded-[35px] rounded-[20px] overflow-hidden aspect-[814/640] bg-no-repeat bg-cover bg-center"
+								style="background-image: url('<?php echo wp_get_attachment_image_url( get_sub_field( 'img' ), 'full' ) ?>')">
+								<div class="relative z-[1] flex flex-col items-center">
+									<?php if ( get_sub_field( 'title' ) ) : ?>
+
+										<div
+											class="max-w-[332px] xl:text-[40px] lg:text-2xl text-xl lg:leading-[1] tracking-[-1px] text-[#000000E0] text-center">
+											<?php the_sub_field( 'title' ); ?>
+										</div>
+									<?php endif; ?>
+									<?php if ( get_sub_field( 'desc' ) ) : ?>
+										<div
+											class="mt-[2px] max-w-[185px] lg:text-[23px] text-sm leading-[1] tracking-[-0.21px] text-[#00000070] text-center">
+											<?php the_sub_field( 'desc' ); ?>
+										</div>
+									<?php endif; ?>
+									<?php
+									$button = get_sub_field( 'button' );
+									$btn_title = ( is_array( $button ) && ! empty( $button['title'] ) ) ? $button['title'] : '';
+									$btn_link = ( is_array( $button ) && ! empty( $button['link'] ) ) ? $button['link'] : '';
+									if ( $btn_title ) : ?>
+
+										<a href="<?php echo check_link( $btn_link ) ?>"
+											class="mt-[22px] block lg:py-[15px] py-2 lg:px-[49px] px-7 rounded-full overflow-hidden bg-primary hover:bg-[#224393c7] text-white font-content lg:text-[15px] text-xs leading-[1] tracking-[-0.151px] w-fit transition-colors duration-300">
+											<?php echo esc_html( $btn_title ); ?>
+										</a>
+									<?php endif; ?>
 								</div>
-								<div
-									class="mt-[2px] max-w-[185px] lg:text-[23px] text-sm leading-[1] tracking-[-0.21px] text-[#00000070] text-center">
-									Thực phẩm bảo vệ sức khỏe
-								</div>
-								<a href=""
-									class="mt-[22px] block lg:py-[15px] py-2 lg:px-[49px] px-7 rounded-full overflow-hidden bg-primary hover:bg-[#224393c7] text-white font-content lg:text-[15px] text-xs leading-[1] tracking-[-0.151px] w-fit transition-colors duration-300">
-									Mua ngay
-								</a>
 							</div>
 						</div>
-					</div>
-				<?php endfor ?>
+					<?php endwhile; ?>
+				</div>
 			</div>
 		</div>
-	</div>
+	<?php endif; ?>
+
 </section>
