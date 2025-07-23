b1414a17454e099ddcdd3288ebb016cd2cc7b6bc
tuannguyen <coixaygio107@gmail.com>
2025-07-23 17:17:24 +0700

    Done html pc, mobile "nghien-cuu-phat-trien"

--- theme/template-pages/nghien-cuu-phat-trien.php
+++ theme/template-pages/nghien-cuu-phat-trien.php
@@ -109,6 +109,85 @@ get_header();
 
     <?php echo get_template_part('/components/block_analysis-slider') ?>
 
+    <section class="lg:py-[121px] py-[60px] bg-[#FFFAFA]">
+        <div class="container">
+            <div class="lg:text-[55px] text-[30px] leading-[1.1] font-semibold text-primary text-center">
+                KẾT QUẢ NGHIÊN CỨU THỰC NGHIỆM VÀ NGHIÊN CỨU LÂM SÀNG
+            </div>
+        </div>
+        <div class="aspect-[1919/937] w-full relative">
+            <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/bg-nkh.png" alt="" loading="lazy"
+                class="absolute inset-0 size-full object-cover">
+        </div>
+        <div class="container">
+            <div class="relative flex flex-col lg:gap-[300px] gap-[200px] items-center">
+                <div class="absolute top-0 left-1/2 -translate-x-1/2 w-[1px] h-full border border-dashed border-black">
+                </div>
+                <div class="relative z-[1] flex flex-col lg:gap-[300px] gap-[200px] items-center">
+                    <div class="max-w-[1186px] text-center text-2xl py-[28px] bg-[#FFFAFA]">
+                        Từ đam mê thảo dược và khát khao giúp đời mãnh liệt, Nhà khoa học Phương Dung đã dành cả đời
+                        nghiên
+                        cứu để tìm ra phương pháp hỗ trợ cải thiện các vấn đề sức khỏe tận gốc, an toàn tuyệt đối cho
+                        người
+                        dùng.
+                    </div>
+                    <?php for ($i = 0; $i < 7; $i++): ?>
+                        <div class="py-[28px] bg-[#FFFAFA] max-w-[859px] flex flex-col items-center">
+                            <div class="lg:text-[40px] text-3xl leading-[1] text-center">
+                                Năm 1998
+                            </div>
+                            <div class="mt-5 lg:text-2xl text-lg text-center text-[#5C5C5C]">
+                                Bà Phương Dung tốt nghiệp Cử nhân ngành Luật
+                                <br> tại Trường Đại học Khoa học xã hội và nhân văn
+                            </div>
+                            <div
+                                class="lg:mt-[50px] mt-4 relative aspect-square md:w-[498px] w-full rounded-[30px] overflow-hidden">
+                                <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/home8.png" alt=""
+                                    loading="lazy" class="absolute inset-0 size-full object-cover">
+                            </div>
+                        </div>
+                    <?php endfor ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="lg:py-[121px] py-[60px] bg-[#FFFAFA]">
+        <div class="container">
+            <div class="space-y-10">
+                <div class="lg:text-[55px] text-[30px] leading-[1.1] font-semibold text-primary text-center">
+                    BẰNG ĐỘC QUYỀN SÁNG CHẾ SỐ 41442
+                </div>
+                <div class="space-y-5 lg:text-2xl">
+                    <p>
+                        KHẲNG ĐỊNH QUY TRÌNH SẢN XUẤT CHẾ PHẨM KIỀM THẢO DƯỢC VÀ CHẾ PHẨM KIỀM THẢO DƯỢC THU ĐƯỢC TỪ QUY
+                        TRÌNH NÀY THUỘC VỀ BÀ NGUYỄN THỊ CHUNG (TỨC NGUYỄN PHƯƠNG DUNG)
+                    </p>
+                    <p>
+                        Sau gần 2 thập kỷ, Nhà khoa học Nguyễn Phương Dung cùng các cộng sự ngày đêm tìm tòi nghiên cứu,
+                        vào tháng 9/2020, Bà chính thức nộp đơn đăng ký bằng độc quyền sáng chế, ròng rã trong 4 năm với
+                        rất nhiều cuộc họp với các nhà khoa học, chuyên gia đầu ngành để bảo vệ thành quả nghiên cứu.
+                        Ngày 18/09/2025, Nhà khoa học Nguyễn Phương Dung chính thức được Cục sở hữu trí tuệ trao quyết
+                        định và Bằng độc quyền sáng chế số 41442 cho "QUY TRÌNH SẢN XUẤT CHẾ PHẨM KIỀM THẢO DƯỢC VÀ CHẾ
+                        PHẨM KIỀM THẢO DƯỢC THU ĐƯỢC TỪ QUY TRÌNH NÀY” có pH cao lành tính có hiệu quả trong việc ức chế
+                        sự phát triển của virus. Khẳng định sự tiên phong trong nghiên cứu khoa học và chất lượng sản
+                        phẩm vượt trội của Kiềm thảo dược đầu tiên và duy nhất tại Việt Nam.
+                    </p>
+                </div>
+            </div>
+            <div class="mt-[71px] flex md:flex-row flex-col lg:gap-10 gap-5 md:items-stretch">
+                <div class="md:w-1/2 w-full aspect-[89/126] relative">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/ketqua.png" alt=""
+                        loading="lazy" class="absolute inset-0 size-full object-cover">
+                </div>
+                <div class="md:w-1/2 w-full aspect-[89/126] relative">
+                    <img src="<?php echo get_stylesheet_directory_uri() ?>/assets/images/ketqua.png" alt=""
+                        loading="lazy" class="absolute inset-0 size-full object-cover">
+                </div>
+            </div>
+        </div>
+    </section>
+
     <?php echo get_template_part('/components/block_partner-slider') ?>
 </main>
 
