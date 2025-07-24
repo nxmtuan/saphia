5104e3454b473a195c7fb48d8de5c2982ca33770
tuannguyen <coixaygio107@gmail.com>
2025-07-24 09:13:25 +0700

    Add contact page template with header and footer

---
+++ theme/template-pages/lien-he.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+Template Name: Liên hệ
+ */
+
+get_header();
+?>
+
+<main>
+    <section class="lg:py-[100px] py-[60px]">
+        <div class="container">
+            <div class="max-w-[841px] mx-auto flex flex-col items-center">
+                <div class="lg:text-4xl text-xl highlight text-center">
+                    Liên hệ với Kiềm Thảo Dược Saphia <br>
+                    <span>để được chuyên gia tư vấn</span>
+                </div>
+                <div class="form-lien-he-saphia"></div>
+            </div>
+        </div>
+    </section>
+</main>
+
+<?php
+get_footer();
