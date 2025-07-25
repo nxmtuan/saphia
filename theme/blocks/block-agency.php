e7638f19e4ddb7cbef04507c454c1aa390508efd
tuannguyen <coixaygio107@gmail.com>
2025-07-25 10:31:26 +0700

    Add accordion styles and update block-agency to use accordion structure

--- theme/blocks/block-agency.php
+++ theme/blocks/block-agency.php
@@ -25,7 +25,7 @@
 
 		<?php if ( have_rows( 'list_content' ) ) : ?>
 			<div
-				class="lg:mt-[60px] mt-10 group md:flex md:w-full md:max-w-full lg:h-[496px] h-[400px] xl:gap-[53px] gap-4 block_slider watch_css center_items md:mx-0 -mx-5">
+				class="accordion-container lg:mt-[60px] mt-10 group md:flex md:w-full md:max-w-full lg:h-[496px] h-[400px] xl:gap-[53px] gap-4 block_slider watch_css center_items md:mx-0 -mx-5">
 				<?php 
                 $i=0;
                 while ( have_rows( 'list_content' ) ) :
@@ -33,7 +33,7 @@
                     $i++;
                     $img_hover = get_sub_field('img_hover')?:get_sub_field('img');
                     ?>
-					<a href="<?php echo check_link(get_sub_field('link')) ?>" target="_blank" class="block_slider-item relative block max-md:min-w-[83%] md:h-full basis-0 <?php echo $i == 1 ? 'md:flex-grow-[2]' : 'md:flex-grow' ?>  lg:rounded-[35px] rounded-[20px] bg-no-repeat bg-cover bg-center cursor-pointer md:transition-all md:duration-500 md:ease-in-out md:group-hover:first:flex-grow md:hover:flex-grow-[2] overflow-hidden group/inner"
+					<a href="<?php echo check_link(get_sub_field('link')) ?>" target="_blank" class="accordion-item block_slider-item relative block max-md:min-w-[83%] md:h-full basis-0 lg:rounded-[35px] rounded-[20px] bg-no-repeat bg-cover bg-center cursor-pointer md:transition-all md:duration-500 md:ease-in-out overflow-hidden group/inner"
 						style="background-image: url('<?php echo wp_get_attachment_image_url(get_sub_field('img'), 'full',false) ?>');">
 
                         <?php echo wp_get_attachment_image($img_hover, 'full',false,['class'=>'absolute inset-0 size-full lg:opacity-0 group-hover/inner:opacity-100 transition-all duration-300 object-cover']) ?>
